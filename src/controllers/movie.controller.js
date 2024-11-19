3e75f6cc9549fd6da4d3f93d5566f49cc0b45ed2
Gleison <gleisonrgomes@gmail.com>
2024-11-19 14:49:32 -0300

    feat: all functions created on controller

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -3,8 +3,16 @@ import { MovieService } from "../services/Movie.service.js";
 const instanceMovieService = new MovieService();
 
 const createMovie = async (req, res) => {
-  const { title, genre, director, synopsis, year_release, actors, watch, trailer } =
-    req.body;
+  const {
+    title,
+    genre,
+    director,
+    synopsis,
+    year_release,
+    actors,
+    watch,
+    trailer,
+  } = req.body;
   const newMovie = await instanceMovieService.createMovieService(
     title,
     genre,
@@ -22,3 +30,37 @@ const getAllMovies = async (req, res) => {
   const movies = await instanceMovieService.getAllMoviesService();
   res.json({ movies });
 };
+
+const getMovieByTitle = async (req, res) => {
+  const { title } = req.query;
+  const movieByTitle = await instanceMovieService.getMovieByTitleService(title);
+  res.json({ movieByTitle });
+};
+
+const getMovieByGenre = async (req, res) => {
+  const { genre } = req.query;
+  const movieByGenre = await instanceMovieService.getMovieByGenreService(genre);
+  res.json({ movieByGenre });
+};
+
+const getMovieByDirector = async (req, res) => {
+  const { director } = req.query;
+  const movieByDirector = await instanceMovieService.getMovieByDirectorService(
+    director
+  );
+  res.json({ director });
+};
+
+const getMovieByActor = async (req, res) => {
+  const { actor } = req.query;
+  const movieByActor = await instanceMovieService.getMovieByActorService(actor);
+  res.json({ movieByActor });
+};
+
+const deleteMovie = async (req, res) => {
+  const { id } = req.query;
+  const deletedMovie = await instanceMovieService.deleteMovieService(id);
+  res.json({ deleteMovie });
+};
+
+export { createMovie, getAllMovies, getMovieByActor, getMovieByDirector, getMovieByGenre, getMovieByTitle, deleteMovie }
