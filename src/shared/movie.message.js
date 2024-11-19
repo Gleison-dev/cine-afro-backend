b71496f96fbe60683f570505a99c0d8b544a23dd
Gleison <gleisonrgomes@gmail.com>
2024-11-18 21:28:50 -0300

    feat: message of movies created

---
+++ src/shared/movie.message.js
@@ -0,0 +1,12 @@
+const ERRORS = {
+  ALREADY_EXISTS: "Filme já cadastrado. Tente novamente!",
+  NOT_FOUND: "Filme não encontrado. Tente novamente!",
+};
+
+const SUCESS = {
+  CREATED: "Filme cadastrado com sucesso!",
+  UPDATED: "Filme atualizado com sucesso!",
+  DELETED: "Filme deletado com sucesso!",
+};
+
+export { ERRORS, SUCESS };
