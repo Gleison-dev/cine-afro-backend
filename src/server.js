31239a77f493076ed76e10518db69137d62a0223
Gleison <gleisonrgomes@gmail.com>
2024-11-19 21:51:47 -0300

    feat: index router imported on server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { testConnection } from "./database/connection.js";
+import { router } from "./routes/index.route.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.port;
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   testConnection();
