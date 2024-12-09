b42941d9c9806785662981dece3fdf4c57aace08
Gleison <gleisonrgomes@gmail.com>
2024-12-09 09:47:04 -0300

    feat: message of movies craeted

---
+++ src/shared/movie.message.js
@@ -0,0 +1,12 @@
+const ERRORS = {
+  ALREADY_EXISTS: "já cadastrado. Tente novamente!",
+  NOT_FOUND: "não encontrado. Tente novamente!",
+};
+
+const SUCESS = {
+  CREATED: "cadastrado com sucesso!",
+  UPDATED: "atualizado com sucesso!",
+  DELETED: "deletado com sucesso!",
+};
+
+export { ERRORS, SUCESS };
